04f30a124160e43d17856ff3fd85faec305dda48
Dzhezar Kadyrov <dzhezik@gmail.com>
2019-03-20 02:50:16 +0200

    Migrations fixed

--- src/Migrations/Version20190320004353.php
+++ src/Migrations/Version20190320004353.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190314012115 extends AbstractMigration
+final class Version20190320004353 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,11 +22,9 @@ final class Version20190314012115 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE photoshoot_image DROP FOREIGN KEY FK_B6068D226EF8C321');
-        $this->addSql('DROP INDEX IDX_B6068D226EF8C321 ON photoshoot_image');
-        $this->addSql('ALTER TABLE photoshoot_image CHANGE photoshot_id photoshoot_id INT NOT NULL');
+        $this->addSql('CREATE TABLE photoshoot_image (id INT AUTO_INCREMENT NOT NULL, photoshoot_id INT NOT NULL, image VARCHAR(255) NOT NULL, INDEX IDX_8DA99ED02191CB92 (photoshoot_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE photoshoot (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, photographer VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, is_posted TINYINT(1) NOT NULL, publication_date DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE photoshoot_image ADD CONSTRAINT FK_8DA99ED02191CB92 FOREIGN KEY (photoshoot_id) REFERENCES photoshoot (id)');
-        $this->addSql('CREATE INDEX IDX_8DA99ED02191CB92 ON photoshoot_image (photoshoot_id)');
     }
 
     public function down(Schema $schema) : void
@@ -35,9 +33,7 @@ final class Version20190314012115 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE photoshoot_image DROP FOREIGN KEY FK_8DA99ED02191CB92');
-        $this->addSql('DROP INDEX IDX_8DA99ED02191CB92 ON photoshoot_image');
-        $this->addSql('ALTER TABLE photoshoot_image CHANGE photoshoot_id photoshot_id INT NOT NULL');
-        $this->addSql('ALTER TABLE photoshoot_image ADD CONSTRAINT FK_B6068D226EF8C321 FOREIGN KEY (photoshot_id) REFERENCES photoshoot (id)');
-        $this->addSql('CREATE INDEX IDX_B6068D226EF8C321 ON photoshoot_image (photoshot_id)');
+        $this->addSql('DROP TABLE photoshoot_image');
+        $this->addSql('DROP TABLE photoshoot');
     }
 }
